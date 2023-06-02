1404ffcb6108c8a935f1bcfd31d829791ec1aa04
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-02 11:10:26 +0530

    Bubble Sort

--- Sorting/Bubble_Sort.cpp
+++ Sorting/Bubble_Sort.cpp
@@ -1,46 +1,49 @@
 #include <iostream>
 using namespace std;
 
+int mergeSort(int arr[], int n)
+{
 
+    for (int i = 1; i < n; i++)
+    {
 
-int mergeSort(int arr[], int n){
+        for (int j = 0; j < n - i; j++)
+        {
 
-    for(int i=1; i<n; i++){
-
-        for(int j=0; j<n-i; j++){
-
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
-
 }
 
+int printSortArr(int arr[], int n)
+{
 
-int printSortArr(int arr[], int n){
-    
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 }
 
-int main(){
+int main()
+{
 
-int arr[100];
-int n; 
-cout<<"Enter the size of the array : ";
-cin>>n;
-cout<<"Enter the element of the array : ";
-for(int i=0; i<n; i++){
-    cin>>arr[i];
-}
+    int arr[100];
+    int n;
+    cout << "Enter the size of the array : ";
+    cin >> n;
+    cout << "Enter the element of the array : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 
-mergeSort(arr,n);
-cout<<"Sorted array is : ";
+    mergeSort(arr, n);
+    cout << "Sorted array is : ";
 
-printSortArr(arr,n);
-
+    printSortArr(arr, n);
 
     return 0;
 }
