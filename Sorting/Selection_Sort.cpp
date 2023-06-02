69b999546b9ff29a20e74240f10bca5137599f4d
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-02 11:09:58 +0530

    Selection Sort

--- Sorting/Selection_Sort.cpp
+++ Sorting/Selection_Sort.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
 using namespace std;
 
+int arrSort(int arr[], int n)
+{
 
-int arrSort(int arr[], int n){
+    for (int i = 0; i < n - 1; i++)
+    {
 
-    for(int i=0; i<n-1; i++){
+        int minIndex = i;
 
-        int minIndex=i;
-
-        for(int j=i+1; j<n; j++){
-            if(arr[j]<arr[minIndex])
-            minIndex=j;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
         }
-        swap(arr[minIndex],arr[i]);
-    }
-    
-    
-}
-
-int printArray(int arr[],int n){
-    for(int i=0; i<n;i++){
-        cout<<arr[i]<<" ";
-        
+        swap(arr[minIndex], arr[i]);
     }
 }
 
-int main(){
+int printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
 
     int arr[100];
     int n;
-    cout<<"enter the size of the array : ";
-    cin>>n;
-    cout<<"enter the elements of the array : ";
-    for(int i=0; i<n;i++){
-        cin>>arr[i];
+    cout << "enter the size of the array : ";
+    cin >> n;
+    cout << "enter the elements of the array : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
 
-    arrSort(arr,n);
-   
-   cout<<"sorted array is : ";
-    printArray(arr,n);
+    arrSort(arr, n);
+
+    cout << "sorted array is : ";
+    printArray(arr, n);
 
     return 0;
 }
