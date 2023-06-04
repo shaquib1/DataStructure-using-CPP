d474599a46dca7cef205d8e688c9f7141a265936
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-04 11:30:06 +0530

    stack

---
+++ STL/Stack.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+
+    stack<string> s;
+    s.push("shaquib");
+    s.push("areesha");
+    s.push("shreya");
+    s.push("prachi");
+
+   cout<<"print top element:"<<s.top()<<endl;
+
+   s.pop();
+
+  // after deleting element check size of stack
+
+  cout<<"size of stack : "<<s.size()<<endl;
+
+  //check stack empty or not 
+
+  cout<<"stack empty or not : "<<s.empty();
+
+    return 0;
+}
