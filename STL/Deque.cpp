587a02f3c1b678fb18999c0a1324d41ed4261ed8
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-04 11:41:39 +0530

    Deque

--- STL/Deque.cpp
+++ STL/Deque.cpp
@@ -1,45 +1,40 @@
 //  you can use push and pop operation on both side on front(starting) and back(ending).
 
-
 #include <iostream>
 #include <deque>
 using namespace std;
 
-int main(){
+int main()
+{
 
+   deque<int> d;
 
-     deque<int> d;
-     
-     d.push_back(3);
-     d.push_front(4);
-     d.push_front(5);
+   d.push_back(3);
+   d.push_front(4);
+   d.push_front(5);
 
-    //for each loop for printing deque
-     for(int i: d){
-        cout<<i<<" ";
-        
-     }
+   // for each loop for printing deque
+   for (int i : d)
+   {
+      cout << i << " ";
+   }
 
-     cout<<endl;
+   cout << endl;
 
-     
-     //d.pop_front();  delete element from starting side 
-     //d.pop_back();  delete element form back side means ending side 
-     cout<<"index 1 element is : "<<d.at(1)<<endl;
-     cout<<"front element is : "<<d.front()<<endl;
-     cout<<"back element is : "<<d.back()<<endl;
-     cout<<"size of the deque : "<<d.size()<<endl;
+   // d.pop_front();  delete element from starting side
+   // d.pop_back();  delete element form back side means ending side
+   cout << "index 1 element is : " << d.at(1) << endl;
+   cout << "front element is : " << d.front() << endl;
+   cout << "back element is : " << d.back() << endl;
+   cout << "size of the deque : " << d.size() << endl;
 
+   // to erase element from deque use erase();
+   // in which you tell you want to erase element in range or single element
 
-    //to erase element from deque use erase();
-    //in which you tell you want to erase element in range or single element
+   d.erase(d.begin(), d.begin() + 2); // it means delete 1st element
 
-    d.erase(d.begin(),d.begin()+2);  //it means delete 1st element
-    
-    //now we check size of the deque we find 1 size is decrease because 1 element is erase 
-      cout<<"after erase size of deque : "<<d.size();
+   // now we check size of the deque we find 1 size is decrease because 1 element is erase
+   cout << "after erase size of deque : " << d.size();
 
-       
-
-    return 0;
+   return 0;
 }
