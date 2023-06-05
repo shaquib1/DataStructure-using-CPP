6ceee8b0fef4492c11f7cdfb6ad2ab20f01203fb
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-05 11:23:02 +0530

    sum of all element

--- Array/Sum_of_All_Elements.cpp
+++ Array/Sum_of_All_Elements.cpp
@@ -9,7 +9,7 @@ int main(){
 
    cout<<"Enter the size of the array: ";
    cin>>x;
-   cout<<"Enter the" << x << "array elements:";
+   cout<<"Enter the elements of the array : "<<endl; 
 
    for(int i=0; i<x; i++){
 
