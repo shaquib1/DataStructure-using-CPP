ad98c828afab86cfd5564dbaf923039ef625ec6a
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-13 11:44:08 +0530

    2d Array

--- Array/2D_Array/2d_Array.cpp
+++ Array/2D_Array/2d_Array.cpp
@@ -7,9 +7,15 @@ using namespace std;
 int main()
 {
 
-    int arr[3][4];
+   // initialization of 2d array
 
-    // taking input from user
+    //int arr[3][4];
+
+   // int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
+
+    int arr[3][4]={{1,2,3,34} , {4,5,6,43} , {7,8,9,44}};
+/*
+    // taking input from user -> row wise input
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 4; j++)
@@ -17,6 +23,18 @@ int main()
             cin >> arr[i][j];
         }
     }
+
+
+
+    // taking input from user -> coloume wise input
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> arr[j][i];
+        }
+    }*/
+
 
     // for printing array
 
