44b3a5db2fec6d03107e2ec0c615405d78bd5c86
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-13 15:45:34 +0530

    Row and coloumn wise add of element

---
+++ Array/2D_Array/Row_&_Coloumn_wise_sum.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+void row_sum(int arr[][4], int i, int j)
+{
+
+    int sum = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            sum += arr[i][j];
+        }
+        cout << sum << endl;
+    }
+}
+void coloum_sum(int arr[][4], int i, int j)
+{
+
+    int sum = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            sum += arr[j][i];
+        }
+        cout << sum<<" ";
+    }
+}
+
+int main()
+{
+
+    int arr[3][4];
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "row wise sum : "<<endl;
+    row_sum(arr, 3, 4);
+
+    cout << "coloumn wise sum: "<<endl;
+    coloum_sum(arr, 3, 4);
+
+    return 0;
+}
