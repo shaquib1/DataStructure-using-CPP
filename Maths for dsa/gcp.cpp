ea73544ff2d658dd2fe1be0a985d4179383c908f
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-25 22:39:16 +0530

    Greatest Common divisor

--- Maths for dsa/gcp.cpp
+++ Maths for dsa/gcp.cpp
@@ -1,12 +1,45 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
+int gcd(int a, int b)
+{
 
+    if (a == 0)
+    {
+        return b;
+    }
 
+    if (b == 0)
+    {
+        return a;
+    }
 
-int main(){
+    while (a != b)
+    {
 
+        if (a > b)
+        {
+            a = a - b;
+        }
+        else
+        {
+            b = b - a;
+        }
+    }
+}
 
+int main()
+{
+
+    int a, b;
+    cout << "Enter the 1st number: ";
+    cin >> a;
+    cout << "Enter the 2nd number: ";
+    cin >> b;
+
+    int ans = gcd(a, b);
+
+    cout << "gcd of " << a << " & " << b << " is : " << ans;
 
     return 0;
 }
