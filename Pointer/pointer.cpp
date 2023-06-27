50700c8798fcc58eea1a61f8c262ad5430f3c479
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-27 22:49:16 +0530

    Pointer

---
+++ Pointer/pointer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int num = 33;
+
+    cout << num << endl;
+
+    // symbol of adress is &
+
+    cout << "Adress of num is : " << &num << endl;
+
+    return 0;
+}
