b16faa152d55836148b5ff6beb1636935af19d08
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-28 23:52:06 +0530

    pointer

--- Pointer/pointer.cpp
+++ Pointer/pointer.cpp
@@ -4,13 +4,22 @@ using namespace std;
 int main()
 {
 
+
+// for checking address of a number
     int num = 33;
 
     cout << num << endl;
 
     // symbol of adress is &
 
-    cout << "Adress of num is : " << &num << endl;
+   // cout << "Adress of num is : " << &num << endl;
+
+    // pointer stores address
+
+    //declaration of a ponter 
+
+    int *ptr=&num;
+    cout<<"address of num :"<<ptr;
 
     return 0;
 }
