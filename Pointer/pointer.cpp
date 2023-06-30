46366816cc32bdd72372c4bb4ea2d8ef266939e2
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-30 23:19:24 +0530

    pointer

--- Pointer/pointer.cpp
+++ Pointer/pointer.cpp
@@ -15,17 +15,28 @@ int main()
    // cout << "Adress of num is : " << &num << endl;
 
     // pointer stores address
-
-    //declaration of a ponter 
-
+ 
+//declaration of a ponter 
     int *ptr=&num;
     cout<<"address of num :"<<ptr<<endl;
     cout<<"this print original value of num :"<<*ptr;
 
     // here num and *ptr give the same value
+    //*ptr means value of address of ptr
+    // ptr means address of a number  
+    
+
     cout<<"address of num: "<<&num<<endl;
     cout<<"value is : "<<*ptr<<endl;
-    cout<<"value of num: "<<num<<endl;
+    cout<<"value is: "<<num<<endl;
+
+
+    // for double value
+    double d=3.4;
+    double *pt=&d;
+
+    cout<<"Address of d:"<<ptr<<endl;
+    cout<<"vaule is: "<<*pt<<endl;
 
     return 0;
 }
