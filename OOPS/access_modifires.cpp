4a23e9770bb179c36c682300bf6801c1a0fa21b3
Md Shaquib Alam <shaquib475@gmail.com>
2023-09-13 23:37:25 +0530

    access modifires

--- OOPS/access_modifires.cpp
+++ OOPS/access_modifires.cpp
@@ -1,33 +1,32 @@
 
 // it means data members where can be access inside class or outof class and scope of data member which describe the access modifires
-//There are three type of access modifires a> public b> private c> protected
-
+// There are three type of access modifires a> public b> private c> protected
 
 #include <iostream>
 using namespace std;
 
-class Car{
+class Car
+{
 
-public:       // it means below this public access modifiers all data member of the class is public means it can be use outof the class and inside the class
-int age;
-char name;
+public: // it means below this public access modifiers all data member of the class is public means it can be use outof the class and inside the class
+    int age;
+    char name;
 
-private:     // it means below the private access modifiers all data member of the class is private means it can be use or modify in inside this class only not out of this class
-int profit;
-char food;
+private: // it means below the private access modifiers all data member of the class is private means it can be use or modify in inside this class only not out of this class
+    int profit;
+    char food;
 
-// but when we not declaring access modifires by default access modifirs is private so decleare your access modifires
-
+    // but when we not declaring access modifires by default access modifirs is private so decleare your access modifires
 };
 
-int main(){
+int main()
+{
 
     Car obj;
-    obj.age= 33;
+    obj.age = 33;
 
-    cout<<"here is public modifires:"<<obj.age<<endl; // age can be accessed because it is public
-   // cout<<"private modifires : "<<obj.food;  // food is privarte so it can't call
-
+    cout << "here is public modifires:" << obj.age << endl;       // age can be accessed because it is public
+    // cout<<"private modifires : "<<obj.food;                   // food is privarte so it can't call
 
     return 0;
 }
