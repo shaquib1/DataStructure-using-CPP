086757e0288887562c20f8fed91b773e062544ce
Md Shaquib Alam <shaquib475@gmail.com>
2023-09-13 23:33:58 +0530

    class and object practice

---
+++ OOPS/object_and_class_practice.cpp
@@ -0,0 +1,38 @@
+
+// c++ class and object example
+
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+public:
+    int id;
+    string name;
+    float salaray;
+
+    void insertValue(int i, string s, float f)
+    {
+        id = i;
+        name = s;
+        salaray = f;
+    }
+
+    void displayFunction()
+    {
+        cout << "id:" << id << " "
+             << "name: " << name << " "
+             << "salary: " << salaray;
+    }
+};
+
+int main(void)
+{
+
+    Employee obj;
+
+    obj.insertValue(11202615, "shaquib", 234543);
+    obj.displayFunction();
+
+    return 0;
+}
