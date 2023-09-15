0372f83dd063370a77e215ab6a1493ffc8135bba
Md Shaquib Alam <shaquib475@gmail.com>
2023-09-15 23:55:13 +0530

    getter and setter

--- OOPS/getter_and_setter.cpp
+++ OOPS/getter_and_setter.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 using namespace std;
 
+class Getter_setter{
+    public:
+   int health;
+
+   private:
+   int name;
+
+
+   int getter(){
+    return name;
+   }
+
+   void setter(int health){
+        health = 234;
+   }
+};
 
 int main(){
 
-    
+// create object of the Getter_setter class
+
+Getter_setter obj;
+
+  cout<<"we can access private data member"<<obj.getter();
 
     return 0;
 }
