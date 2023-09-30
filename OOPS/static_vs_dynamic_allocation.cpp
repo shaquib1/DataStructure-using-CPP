18e67aecd3652b073dc500eef85e494058778b84
Md Shaquib Alam <shaquib475@gmail.com>
2023-09-30 22:24:29 +0530

    static and dynamic allocation of creating object

--- OOPS/static_vs_dynamic_allocation.cpp
+++ OOPS/static_vs_dynamic_allocation.cpp
@@ -4,22 +4,49 @@
 using namespace std;
 
 class Hero{
+
+// datamembers
+
     private:
-    int rate;
+   int health;
+
+   public:
+   int level;
+   int test;
 
 
-    public:
-    int success;
+   int getHealth(){   // creating getter funciton 
+    return health;
+   }
+
+   int getLevel(){
+        return level;
+   }
+
+   void setterHealth(int h){    // creating setter function
+       health = h;
+   }
+
+   void setterLevel(int l){
+    level =l;
+   }
 };
 
 int main(){
 
     // static allocation of creating object
-    Hero obj;
-
+    Hero a;
+     a.setterHealth(12);
+    cout<<"health is:"<<a.getHealth()<<endl;
 
     // dynamic allocation of creating object
     Hero *obj = new Hero;
+    obj->setterHealth(432);
+
+    obj->test=89;
+      cout<<"so test value is:"<<obj->test<<endl;
+     cout<<"Health is:"<<(*obj).getHealth()<<endl;
+
 
     return 0;
 }
