3405ceba392178bcf473755304dec2952f540afa
Md Shaquib Alam <shaquib475@gmail.com>
2023-10-02 17:12:17 +0530

    parameterized constructor

---
+++ OOPS/constructor/parameterized_constructor.cpp
@@ -0,0 +1,34 @@
+
+
+// a constructor which has parameters is called parameterized constructor.
+// it is used to initialize the data members of an object to specific.
+
+#include <iostream>
+using namespace std;
+
+class Employee{
+
+public:
+int id;
+string name;
+
+Employee(int i , string n){
+    id=i;
+    name=n;
+}
+
+void print(){
+    cout<<"id is : "<<id<<endl;
+    cout<<"name is : "<<name<<endl;
+}
+
+};
+
+int main(){
+ 
+ Employee obj(11202615 , "shaquib");
+
+obj.print();
+
+    return 0;
+}
