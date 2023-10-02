a39cf331d4644ab3d1d2527a8015b96a0f0aa851
Md Shaquib Alam <shaquib475@gmail.com>
2023-10-02 12:32:52 +0530

    Constructor

--- OOPS/constructor.cpp
+++ OOPS/constructor.cpp
@@ -1,27 +1,42 @@
+
+// Cosntructor is a special method that is authomatically called when an object of a class is created.
 // when we create an object then constructor called and the constructor is same as class name
-//
+//to create a constructor , use the same name as the class, followed by parentheses()
+
+/*some characteristic of constructor are:
+
+-> the name of constructor is same as its class name.
+-> it is always public - means constructors are mostly declared in the public section of the class
+-> it does not have return value; hence they don't have a return type.
+->constructor gets called automatically when we create the object of the class.
+-> constructor can be overloaded.
+
+*/
 
 
+/*  Types of construtor mainly
+
+-> Default construtor
+-> Parameterized constructor
+-> Copy constructor
+-> overloaded constructor
+*/
 
 #include <iostream>
 using namespace std;
 
 class Myclass{
-    public:
+    public:    // Access specifier
 
-    string name;
-    int age;
-
-  void testing(){
-    name= "shaquib";
-    age=34;
+   Myclass(){     // constructor  
+        cout<<"constructor created"
   }
 };
 
 int main(){
 
- Myclass myobj;
+ Myclass myobj;    // create an object of myclass (this will call the construtor automatically)
 
- myobj.testing();
+
     return 0;
 }
