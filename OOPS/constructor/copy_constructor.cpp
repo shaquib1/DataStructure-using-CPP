0fefd71ba21068bc7126acbbbf5ffd241a8d118c
Md Shaquib Alam <shaquib475@gmail.com>
2023-10-03 12:30:59 +0530

    copy constructor

---
+++ OOPS/constructor/copy_constructor.cpp
@@ -0,0 +1,24 @@
+//copy constructor
+
+
+
+#include <iostream>
+using namespace std;
+
+
+class Hero{
+    
+    public:
+
+    string name;
+    string location;
+
+};
+
+
+int main(){
+
+ Hero obj;
+
+    return 0;
+}
