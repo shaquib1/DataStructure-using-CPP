48f9e7a6d53752051fa1ea9a6c623db973bac2b5
Md Shaquib Alam <shaquib475@gmail.com>
2023-10-03 18:24:04 +0530

    copy constructor

--- OOPS/constructor/copy_constructor.cpp
+++ OOPS/constructor/copy_constructor.cpp
@@ -1,5 +1,9 @@
-//copy constructor
+//copy constructor is used to copy data of one object to another 
+// its two type 
+// 1> default copy constructor - when a class create then compilor create the default copy constructor
+// 2> user defined constructor - user can defines the user-defined constructor
 
+// -> vvi point ->  in this we use "pass by reference " because when we pass the constructor then an infinity loop occur
 
 
 #include <iostream>
@@ -8,17 +12,44 @@ using namespace std;
 
 class Hero{
     
-    public:
+    private:
+    int length;
+    int height;
 
-    string name;
-    string location;
+  public:
+
+  Hero(int len , int hgt){
+    length = len;
+    height = hgt;
+  }
+
+    //copy constructor with Hero object as parameter
+    // copies data of the obj parameter
+    Hero(Hero & obj){
+        length = obj.length;
+        height = obj.height;
+    }
+
+    int print_obj(){
+       
+    return height * length;
+    }
 
 };
 
 
 int main(){
 
- Hero obj;
+  Hero obj1(7 , 3);
+
+cout<<"area of obj1:"<<obj1.print_obj()<<endl;
+
+
+// copy constructor
+ Hero obj2(obj1);
+ cout<<"area of obj2 :"<<obj2.print_obj();
 
     return 0;
 }
+
+
