0b26542646bce7b78028b80cabe220cb718bf382
Md Shaquib Alam <shaquib475@gmail.com>
2023-10-03 11:52:03 +0530

    this keyword

--- OOPS/this_keyword.cpp
+++ OOPS/this_keyword.cpp
@@ -20,6 +20,9 @@ class Test{
     this->name=name;
   }
 
+Test(){
+  cout<<"address of this:"<<this<<endl;    // this is for testing of address of this keyword and current object is equal
+}
 
    void print(){
     cout<<"use of this keyword"<<endl;
@@ -31,6 +34,9 @@ class Test{
 int main(){
 
 Test obj(11202615 , "shaquib");
+
+Test b;
+cout<<"address of b:"<< &b<<endl;  // so we make a new obj to check address of this keyword and current obj
 
 obj.print();
 
