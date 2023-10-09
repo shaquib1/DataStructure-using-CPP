1f7539139193e2f9ff2b77a79a28f5be95009773
Md Shaquib Alam <shaquib475@gmail.com>
2023-10-09 18:57:30 +0530

    destructure

--- OOPS/constructor/destructure.cpp
+++ OOPS/constructor/destructure.cpp
@@ -1,9 +1,51 @@
+//destructure means deallocate(free) the memory
+// -> when scope of the object is end than destructure called of the object 
+// when a class is created then its destructure also created  (also user can create the destructure)
+ // same name as class name
+ // no  any return type
+ // no input parameter
+
+
+
+
 #include <iostream> 
 using namespace std;
 
+class Hero{
+
+    private:
+    int age;
+    char name;
+
+
+  public:
+  
+   // this is constructor 
+    Hero(){
+     cout<<"constructor is called"<<endl;
+
+    }
+
+
+    // destructure 
+    ~Hero(){
+        cout<<"destructor is called"<<endl;
+    }
+
+};
 
 int main(){
 
+    // static allocation to create object
+     Hero obj1;    // for static allocation destructure automatically called
+     Hero obj2;
+
+
+     // dynamic allocation to creating object
+    // Hero *a1 = new Hero();      // for dynamic allocation destructure manually  called 
+     Hero *a2 = new Hero();
+
+    // delete a2;
 
     return 0;
 }
