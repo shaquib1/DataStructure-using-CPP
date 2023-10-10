ab658fa163744c6e93d0adea1557d032d3f9efc0
Md Shaquib Alam <shaquib475@gmail.com>
2023-10-10 12:58:27 +0530

    destructor

--- OOPS/constructor/destructure.cpp
+++ OOPS/constructor/destructure.cpp
@@ -1,50 +1,47 @@
-//destructure means deallocate(free) the memory
-// -> when scope of the object is end than destructure called of the object 
-// when a class is created then its destructure also created  (also user can create the destructure)
- // same name as class name
- // no  any return type
- // no input parameter
- // same as it is called at once same as constructor
+// destructure means deallocate(free) the memory
+//  -> when scope of the object is end than destructure called of the object
+//  when a class is created then its destructure also created  (also user can create the destructure)
+//  same name as class name
+//  no  any return type
+//  no input parameter
+//  same as it is called at once same as constructor
 
 
 
-
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
-class Hero{
+class Hero
+{
 
-    private:
+private:
     int age;
     char name;
 
-
-  public:
-  
-   // this is constructor 
-    Hero(){
-     cout<<"constructor is called"<<endl;
-
+public:
+    // this is constructor
+    Hero()
+    {
+        cout << "constructor is called" << endl;
     }
 
-
-    // destructure 
-    ~Hero(){
-        cout<<"destructor is called"<<endl;
+    // destructure
+    ~Hero()
+    {
+        cout << "destructor is called" << endl;
     }
-
 };
 
-int main(){
+int main()
+{
 
     // static allocation to create object
-     Hero obj1;    // for static allocation destructure automatically called
-     Hero obj2;
+    Hero obj1; // for static allocation destructure automatically called
+    Hero obj2;
 
-
-     // dynamic allocation to creating object
-    // Hero *a1 = new Hero();      // for dynamic allocation destructure manually  called 
-     //Hero *a2 = new Hero();
+    // dynamic allocation to creating object
+    // Hero *a1 = new Hero();      // for dynamic allocation destructure manually  called
+    // Hero *a2 = new Hero();
 
     // delete a2;
 
