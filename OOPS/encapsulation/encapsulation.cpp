e4b89a8579db2e01fd724ee5ed5b3ac144ddf7ef
Md Shaquib Alam <shaquib475@gmail.com>
2023-10-13 23:12:26 +0530

    encapsulation

---
+++ OOPS/encapsulation/encapsulation.cpp
@@ -0,0 +1,24 @@
+
+//******** encapsulation  means->synonyms-> information hiding or data hiding*****//
+
+// wrapping up the datamembers and functions inside a single entity(class)
+
+// fully encapsulated class-> when all datamembers are private then fully encapsulated achieved. so these datamembers are not accessed by other class .
+
+/* advantages -> 1. data hiding -> security increase
+                 2. if we want , we can make class " Read Only"
+                 3. code reuseability
+                 4. encapsulation helps in Unit testing
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+
+
+    return 0;
+}
