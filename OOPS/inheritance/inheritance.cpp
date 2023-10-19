9f279216e3d49088c47b3b670d4c714b870f169d
Md Shaquib Alam <shaquib475@gmail.com>
2023-10-19 23:38:27 +0530

    inheritance

--- OOPS/inheritance/inheritance.cpp
+++ OOPS/inheritance/inheritance.cpp
@@ -1,16 +1,34 @@
 
 
-//
+// 
 
 
 
 #include <iostream>
 using namespace std;
 
+class human{
+    public:
+    int height;
+    int age;
+
+    
+};
+
+class male: public human{
+
+    public:
+    string name;
+
+};
+
 
 
 int main(){
 
+  male obj;
+
+  cout<<obj.age<<endl;
 
 
     return 0;
