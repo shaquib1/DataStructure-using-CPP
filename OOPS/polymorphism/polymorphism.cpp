41670faf6627d740f2a83df66f9d2d6e9da88803
Md Shaquib Alam <shaquib475@gmail.com>
2023-10-26 23:51:57 +0530

    polymorphism

--- OOPS/polymorphism/polymorphism.cpp
+++ OOPS/polymorphism/polymorphism.cpp
@@ -1,8 +1,12 @@
 
+// polymorphism means -> many forms 
+// polymorphism is a concept that allows you to perform a single action in different ways.
+// in real life example -> A person at the same time can have different characteristics. 
+// -> Like a man at the same time is a fater , a husband , a son , and an employee. so the same person have different behaior in different situation. called polymorphism.
 
-
-// polymorphism means more forms 
-// it is two type 1> compile time   2> Run time
+// there are two types of polymorphism in cpp
+ //  -> 1> Compile time polymorphism -> it is also two types -> a> function overloading   b> operator overloading
+//  -> 2> Run time polymorphism  -> it is one type  a> function overriding || virtual funciton 
 
 #include <iostream>
 using namespace std;
