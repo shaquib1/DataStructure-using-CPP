ec988060b7752f7a172c7baff8c5e1af21ab1299
Md Shaquib Alam <shaquib475@gmail.com>
2023-10-30 11:23:37 +0530

    Access modifires | Mode of inheritance

--- OOPS/inheritance/access_modifires_and_inheritance.cpp
+++ OOPS/inheritance/access_modifires_and_inheritance.cpp
@@ -1,5 +1,6 @@
 
 
+
 // Mode of inheritance 
 
 // 1. Public mode:If we derive a subclass from a publicbase class. Then, the base class’s public members will become public in the derived class, 
@@ -24,3 +25,40 @@ Base Clss member Access Specifier      Public                    Protected      
 
 
 */
+
+
+
+// ex of this 
+
+// C++ Implementation to show that a derived class 
+// doesn’t inherit access to private data members. 
+// However, it does inherit a full parent object. 
+class A { 
+public: 
+	int x; 
+
+protected: 
+	int y; 
+
+private: 
+	int z; 
+}; 
+
+class B : public A { 
+	// x is public 
+	// y is protected 
+	// z is not accessible from B 
+}; 
+
+class C : protected A { 
+	// x is protected 
+	// y is protected 
+	// z is not accessible from C 
+}; 
+
+class D : private A // 'private' is default for classes 
+{ 
+	// x is private 
+	// y is private 
+	// z is not accessible from D 
+};
