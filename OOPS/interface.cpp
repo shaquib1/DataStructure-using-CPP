e7ce99b8e13e78914479ab71cf72dc1dd28b190f
Md Shaquib Alam <shaquib475@gmail.com>
2023-11-08 20:29:06 +0530

    interface

--- OOPS/interface.cpp
+++ OOPS/interface.cpp
@@ -1,2 +1,51 @@
 
-// in this file we undertand about interface
+// in this file we undertand about interf
+
+#include<iostream>
+using namespace std;
+// parent class
+class Shape 
+{
+  public:
+  // pure virtual function providing interface framework.
+  virtual int getArea() = 0;
+  void setWidth(int w) {
+    width = w;
+  }
+  void setHeight(int h) {
+   height = h;
+  }
+  protected:
+  int width;
+  int height;
+};
+// child classes for implementation
+class Rectangle: public Shape {
+  public:
+  int getArea() { 
+   return (width * height); 
+  }
+};
+// child classes for implementation
+class Triangle: public Shape {
+  public:
+  int getArea() { 
+    return (width * height)/2; 
+  }
+};
+int main()
+{
+  Rectangle Rect;
+  Triangle Tri;
+  Rect.setWidth();
+  Rect.setHeight(4);
+  // display the area of the object.
+  cout << "Total Rectangle area: " << Rect.getArea() << endl;
+
+  Tri.setWidth(8);
+  Tri.setHeight(4);
+
+  // Print the area of the object.
+  cout << "Total Triangle area: " << Tri.getArea() << endl;
+  return 0;
+}
