100c8b0e0d7e3f18d655d57244d2402d63e20139
Md Shaquib Alam <shaquib475@gmail.com>
2023-11-16 22:59:26 +0530

    virtual function

--- OOPS/virtual_function_&_class/virtual_class.cpp
+++ OOPS/virtual_function_&_class/virtual_class.cpp
@@ -9,3 +9,39 @@
 // class C : public virtual A
 // {
 // };
+
+
+// C++ program to illustrate
+// concept of Virtual Functions
+
+// #include <iostream>
+// using namespace std;
+
+// class base {
+// public:
+// 	virtual void print() { cout << "print base class\n"; }
+
+// 	void show() { cout << "show base class\n"; }
+// };
+
+// class derived : public base {
+// public:
+// 	void print() { cout << "print derived class\n"; }
+
+// 	void show() { cout << "show derived class\n"; }
+// };
+
+// int main()
+// {
+// 	base* bptr;
+// 	derived d;
+// 	bptr = &d;
+
+// 	// Virtual function, binded at runtime
+// 	bptr->print();
+
+// 	// Non-virtual function, binded at compile time
+// 	bptr->show();
+
+// 	return 0;
+// }
