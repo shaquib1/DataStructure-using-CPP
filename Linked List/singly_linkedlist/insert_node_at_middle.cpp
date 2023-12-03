a9bee916310f933d859bd14fda78f8eba8f3a915
Md Shaquib Alam <shaquib475@gmail.com>
2023-12-03 22:06:17 +0530

    insert node at middle of the linkedlist

--- Linked List/singly_linkedlist/insert_node_at_middle.cpp
+++ Linked List/singly_linkedlist/insert_node_at_middle.cpp
@@ -1,93 +1,92 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node {
+class Node
+{
 
-    public:
+public:
     int data;
-    Node* next;
+    Node *next;
 
-    //constructor
-    Node(int data) {
-        this -> data = data;
-        this -> next = NULL;
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
     }
-
-    
-
 };
 
-void insertAtHead(Node* &head, int d) {
-
+void insertAtTail(Node *&tail, int d)
+{
     // new node create
-    Node* temp = new Node(d);
-    temp -> next = head;
-    head = temp;
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
 }
 
-void insertAtTail(Node* &tail, int d) {
-     // new node create
-    Node* temp = new Node(d);
-    tail -> next = temp;
-    tail  = temp;
-}
+void print(Node *&head)
+{
 
-void print(Node* &head) {
-
-    if(head == NULL) {
-        cout << "List is empty "<< endl;
-        return ;
+    if (head == NULL)
+    {
+        cout << "List is empty " << endl;
+        return;
     }
-    Node* temp = head;
+    Node *temp = head;
 
-    while(temp != NULL ) {
-        cout << temp -> data << " ";
-        temp = temp -> next;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
     cout << endl;
 }
 
-void insertAtPosition( Node* &head, int position, int d) {
+void insertAtPosition(Node *&head, int position, int d)
+{
 
-
-    Node* temp  = head;
+    Node *temp = head;
     int cnt = 1;
 
-    while(cnt < position-1) {
+    while (cnt < position - 1)
+    {
         temp = temp->next;
         cnt++;
     }
 
-    
+    // create a node for d
+    Node *nodeToInsert = new Node(d);
+
+    nodeToInsert->next = temp->next;
+
+    temp->next = nodeToInsert;
 }
 
+int main()
+{
 
+    // created a new node
+    Node *node1 = new Node(10);
+    // cout << node1 -> data << endl;
+    // cout << node1 -> next << endl;
 
-
-int main() {
-    
-    //created a new node
-    Node* node1 = new Node(10);
-    //cout << node1 -> data << endl;
-   // cout << node1 -> next << endl;
-    
-    //head pointed to node1
-    Node* head = node1; 
-    Node* tail = node1;
+    // head pointed to node1
+    Node *head = node1;
+    Node *tail = node1;
     print(head);
 
     insertAtTail(tail, 12);
 
     print(head);
-    
+
     insertAtTail(tail, 15);
     print(head);
 
     insertAtPosition(head, 3, 22);
-    print(head);    
+    print(head);
 
-  
-    
+    insertAtPosition(head, 2, 65);
+    print(head);
 
     return 0;
 }
