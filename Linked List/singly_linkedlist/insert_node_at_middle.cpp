2c3d9b66ea072e39f88a58d237b86006111b918c
Md Shaquib Alam <shaquib475@gmail.com>
2023-12-03 21:14:28 +0530

    insert node at middle of the linkedlist

---
+++ Linked List/singly_linkedlist/insert_node_at_middle.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+
+    public:
+    int data;
+    Node* next;
+
+    //constructor
+    Node(int data) {
+        this -> data = data;
+        this -> next = NULL;
+    }
+
+    
+
+};
+
+void insertAtHead(Node* &head, int d) {
+
+    // new node create
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void insertAtTail(Node* &tail, int d) {
+     // new node create
+    Node* temp = new Node(d);
+    tail -> next = temp;
+    tail  = temp;
+}
+
+void print(Node* &head) {
+
+    if(head == NULL) {
+        cout << "List is empty "<< endl;
+        return ;
+    }
+    Node* temp = head;
+
+    while(temp != NULL ) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+void insertAtPosition( Node* &head, int position, int d) {
+
+
+    Node* temp  = head;
+    int cnt = 1;
+
+    while(cnt < position-1) {
+        temp = temp->next;
+        cnt++;
+    }
+
+    
+}
+
+
+
+
+int main() {
+    
+    //created a new node
+    Node* node1 = new Node(10);
+    //cout << node1 -> data << endl;
+   // cout << node1 -> next << endl;
+    
+    //head pointed to node1
+    Node* head = node1; 
+    Node* tail = node1;
+    print(head);
+
+    insertAtTail(tail, 12);
+
+    print(head);
+    
+    insertAtTail(tail, 15);
+    print(head);
+
+    insertAtPosition(head, 3, 22);
+    print(head);    
+
+  
+    
+
+    return 0;
+}
