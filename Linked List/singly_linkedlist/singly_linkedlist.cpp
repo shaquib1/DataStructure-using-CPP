722e24c21a530fb63525b2384dcc861e7b3c208d
Md Shaquib Alam <shaquib475@gmail.com>
2023-12-03 16:02:39 +0530

    singly linkedlist

--- Linked List/singly_linkedlist/singly_linkedlist.cpp
+++ Linked List/singly_linkedlist/singly_linkedlist.cpp
@@ -1,33 +1,34 @@
 
-// in this only one pointer to go 
+// in this only one pointer to go
 // 1st node called Head
 // each node or element is connect only to its next next node/element using a pointer
-    
+
 #include <iostream>
 using namespace std;
- 
-class Node{
-    public:
-    int data;
-    Node* next;
 
-   // constructor
-    Node(int data){
-     this->data=data;
-     this->next=NULL;
-    }
+class Node
+{
+public:
+  int data;
+  Node *next;
+
+  // constructor
+  Node(int data)
+  {
+    this->data = data;
+    this->next = NULL;
+  }
 };
 
+int main()
+{
 
+  // in this we create dynamically allocation object
 
-int main(){
-
-  //in this we create dynamically allocation object
-    
   Node *node1 = new Node(12);
-  
-  cout<<node1->data<<endl;
-  cout<<node1->next<<endl;
 
-    return 0;
+  cout << node1->data << endl;
+  cout << node1->next << endl;
+
+  return 0;
 }
