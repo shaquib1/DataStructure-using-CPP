efdd2854a83c29dfee5ab9d830c829024cca0ffd
Md Shaquib Alam <shaquib475@gmail.com>
2023-12-12 23:13:15 +0530

    abstraction

--- OOPS/abstraction/abstraction.cpp
+++ OOPS/abstraction/abstraction.cpp
@@ -2,14 +2,14 @@
 
 // real life-example - email , atm , driving a car
 
-//Data abstraction is one of the most essential and important features of object-oriented programming in C++. 
-//Abstraction means displaying only essential information and hiding the implementation details.
+// Data abstraction is one of the most essential and important features of object-oriented programming in C++.
+// Abstraction means displaying only essential information and hiding the implementation details.
 
-// way to achive abtraction :  -> by classses and access specifiers  
-//There are no differences between the specifiers and modifiers, both are same
+// way to achive abtraction :  -> by classses and access specifiers
+// There are no differences between the specifiers and modifiers, both are same
 
 //-----------Advantages of Abstraction---------------
-//  -> Only you can make changes to your data or function, and no one else can. 
+//  -> Only you can make changes to your data or function, and no one else can.
 //  -> It makes the application secure by not allowing anyone else to see the background details.
 //  -> Helps the user to avoid writing the low-level code
 //  -> Avoids code duplication and increases reusability.
@@ -17,33 +17,33 @@
 //  ->Helps to increase the security of an application or program as only important details are provided to the user.
 //  ->It reduces the complexity as well as the redundancy of the code, therefore increasing the readability.
 
-
-// Ex of abstraction 
+// Ex of abstraction
 
 #include <iostream>
 using namespace std;
 
-class abstraction{
+class abstraction
+{
 
-    private:
+private:
+    int x, y;
 
-   int x  , y;
-
-   public:
-
-    void setData( int a , int b){
-      x=a;
-      y=b;
+public:
+    void setData(int a, int b)
+    {
+        x = a;
+        y = b;
     }
 
-    void printValues(){
-        cout<<"x="<<x<<endl;
-        cout<<"y="<<y<<endl;
+    void printValues()
+    {
+        cout << "x=" << x << endl;
+        cout << "y=" << y << endl;
     }
 };
 
-
-int main(){
+int main()
+{
 
     abstraction obj;
     obj.setData(34, 44);
