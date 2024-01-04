aefba930fcaf8ba5b53f05160ae8ea731c6397d0
Md Shaquib Alam <shaquib475@gmail.com>
2024-01-04 22:40:47 +0530

    practice

--- Array/practice.cpp
+++ Array/practice.cpp
@@ -1,16 +1,21 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 
 int main(){
     
-     string s = "shaquib";
+    //  string s = "shaquib";
+    //  sort(s.begin() , s.end());
+
+    //  for(int i=0; i<s.size(); i++){
+    //    cout<<s[i]<<" ";
+    //  }
+
+
+     string s= "34bab986s";
      sort(s.begin() , s.end());
 
-     for(int i=0; i<s.size(); i++){
-       cout<<s[i]<<" ";
-     }
-
+     cout<<"string is:"<<s;
 
 
 
