cdf8187da149d66cdb26ddb8c1fb41972b0458cf
Md Shaquib Alam <shaquib475@gmail.com>
2024-01-14 23:51:36 +0530

    static and dynamic allocation of creating of object in C++

--- OOPS/static_vs_dynamic_allocation.cpp
+++ OOPS/static_vs_dynamic_allocation.cpp
@@ -3,50 +3,55 @@
 #include <iostream>
 using namespace std;
 
-class Hero{
+class Hero
+{
 
-// datamembers
+    // datamembers
 
-    private:
-   int health;
+private:
+    int health;
 
-   public:
-   int level;
-   int test;
+public:
+    int level;
+    int test;
 
+    int getHealth()
+    { // creating getter funciton
+        return health;
+    }
 
-   int getHealth(){   // creating getter funciton 
-    return health;
-   }
-
-   int getLevel(){
+    int getLevel()
+    { 
         return level;
-   }
+    }
 
-   void setterHealth(int h){    // creating setter function
-       health = h;
-   }
+    void setterHealth(int h)
+    { // creating setter function
+        health = h;
+    }
 
-   void setterLevel(int l){
-    level =l;
-   }
+    void setterLevel(int l)
+    {
+        level = l;
+    }
 };
 
-int main(){
+int main()
+{
 
     // static allocation of creating object
     Hero a;
-     a.setterHealth(12);
-    cout<<"health is:"<<a.getHealth()<<endl;
+    a.setterHealth(12);
+    cout << "health is:" << a.getHealth() << endl;
 
-    // dynamic allocation of creating object 
+    // dynamic allocation of creating object
     Hero *obj = new Hero;
     obj->setterHealth(432);
+   // (*obj).setterHealth(333); 2nd method to call 
 
-    obj->test=89;
-      cout<<"so test value is:"<<obj->test<<endl;
-     cout<<"Health is:"<<(*obj).getHealth()<<endl;
-
+    obj->test = 89;
+    cout << "so test value is:" << obj->test << endl;
+    cout << "Health is:" << (*obj).getHealth() << endl;
 
     return 0;
 }
