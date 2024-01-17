705fc402b617dd108161971d3f9f78bcce84f4d3
Md Shaquib Alam <shaquib475@gmail.com>
2024-01-17 23:42:02 +0530

    practice

--- Array/practice.cpp
+++ Array/practice.cpp
@@ -25,26 +25,26 @@ int main(){
 
     // cout<<"ans is:"<<c;
 
-    int arr[10]={1,2,3,4,5,6,6,7,8,9};
+    int arr[10]={1,2,3,4,5,6,6,3,8,9};
     int n=10;
-    map<int,int>mp;
+    unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
             mp[arr[i]]++;
         }
 
-      for(int i=0;i<n;i++){
+      for(auto i:mp){
            // if(mp[arr[i]]==1)return arr[i];
-            cout<<mp[arr[i]]<<" "<<endl;
+            cout<<i.first<<" ";
         }
 
 
     // cout<<"ans is:"<<19/3;
 
 
-    string s="shb";
-    cout<<"size of s:"<<sizeof(s);
+    // string s="shb";
+    // cout<<"size of s:"<<sizeof(s);
 
-    cout<<"ans is:"<<15/2;
+    // cout<<"ans is:"<<15/2;
 
 
     return 0;
