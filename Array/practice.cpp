592137c6a59405e12446e9e62bd741631c66375d
Md Shaquib Alam <shaquib475@gmail.com>
2024-01-26 21:25:49 +0530

    practice

--- Array/practice.cpp
+++ Array/practice.cpp
@@ -1,9 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+int main()
+{
 
-int main(){
-    
     //  string s = "shaquib";
     //  sort(s.begin() , s.end());
 
@@ -11,12 +11,10 @@ int main(){
     //    cout<<s[i]<<" ";
     //  }
 
-
     //  string s= "34bab986s";
     //  sort(s.begin() , s.end());
 
     //  cout<<"string is:"<<s;
-
 
     // int s=0;
     // int b=12;
@@ -25,27 +23,26 @@ int main(){
 
     // cout<<"ans is:"<<c;
 
-    int arr[10]={1,2,3,4,5,6,6,3,8,9};
-    int n=10;
-    unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            mp[arr[i]]++;
-        }
+    int arr[10] = {1, 2, 3, 4, 5, 6, 6, 3, 8, 9};
+    int n = 10;
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        mp[arr[i]]++;
+    }
 
-      for(auto i:mp){
-           // if(mp[arr[i]]==1)return arr[i];
-            cout<<i.second<<" ";
-        }
-
+    for (auto i : mp)
+    {
+        // if(mp[arr[i]]==1)return arr[i];
+        cout << i.second << " ";
+    }
 
     // cout<<"ans is:"<<19/3;
-
 
     // string s="shb";
     // cout<<"size of s:"<<sizeof(s);
 
     // cout<<"ans is:"<<15/2;
-
 
     return 0;
 }
